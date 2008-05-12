9695c81b7cf19deaa2e45997399a26261b4ceda1
Jonathan Rockway <jon@jrock.us>
2008-05-12 14:43:59 +0900

    let's just get rid of this

--- lib/MooseX/Storage/Directory.pm
+++ lib/MooseX/Storage/Directory.pm
@@ -62,42 +62,6 @@ sub store {
     return $object->get_id;
 }
 
-# sub _lockfile {
-#     my ($self) = @_;
-#     return $self->directory->file('lock');
-# }
-
-# sub lock {
-#     my ($self) = @_;
-#     # XXX: this should block or something, but I don't feel like
-#     # writing that code right now
-#     my $lock = $self->_lockfile;
-#     $self->check_lock;
-#     open my $fh, '>', $lock or die "Cannot open lockfile $lock for writing: $!";
-#     print {$fh} "$$\n";
-#     close $fh;
-# }
-
-# # add C< before qw/lookup store/ => sub { shift->asset_lock } > if you care
-# # i only care about locking for index updates
-# sub assert_lock {
-#     my ($self) = @_;
-#     my $lock = $self->_lockfile;
-#     die $self->directory. ' is locked' if -e $lock;
-#     return 1;
-# }
-
-# sub unlock {
-#     my ($self) = @_;
-#     my $lock = $self->_lockfile;
-#     die $self->directory. ' is not locked' unless -e $lock;
-#     open my $fh, '<', $lock or die "Cannot open lockfile $lock for reading: $!";
-#     chomp(my $pid = do { local $/; <$fh> });
-#     die 'I did not lock '. $self->directory. ', so I cannot unlock it'
-#       unless $pid == $$;
-#     unlink $lock or die "Could not unlink $lock: $!";
-# }
-
 sub _index {
     my ($self, $object) = @_;
     my $index_file = $self->directory->file('.index');
