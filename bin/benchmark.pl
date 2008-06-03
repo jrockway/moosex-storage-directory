c66cdb42b3ab230553ada8f115d40f73bcd44f04
Jonathan Rockway <jon@jrock.us>
2008-06-03 16:06:25 -0500

    cleanup benchmark script

--- bin/benchmark.pl
+++ bin/benchmark.pl
@@ -25,18 +25,17 @@ my $storage = MooseX::Storage::Directory->new(
     class     => Class->meta,
     directory => "$dir",
 );
+say "Connected to $dir.";
 
 Class->meta->make_immutable;
 
 my $RAND_MAX = 100_000;
 
 my @random_numbers = shuffle(1..$RAND_MAX);
-my @intervals = ( 10, map { int ($_ * 1000) }(.1, .5, 1..100) );
+my @intervals = ( map { int ($_ * 1000) }(.1, .5, 1..100) );
 my $last = 0;
 my $next = 10;
-my $i = 0;
-my ($hit, $miss) = (0,0);
-
+my $i = shift @ARGV || 0;
 $|++;
 push @intervals, 0;
 open my $datafile, '>', 'out' or die "datafile open: $!";
@@ -44,7 +43,7 @@ while(1){
     my $how_many = $next-$last; $last = $next; $next = shift @intervals;
 
     # create objects
-    for(1..$how_many){ ### Creating $how_many objects [%]
+    for(1..$how_many){ ### Creating $how_many objects ($last total) [%]
         my $obj = Class->new( id => $i++, num => shift @random_numbers );
         $storage->store($obj);
     }
@@ -52,15 +51,12 @@ while(1){
     # search
     print {$datafile} "$last, ";
     my $start = time;
-    for(1..3000){ ### Running 3000 searches on $last objects [%]
+    for(1..50000){ ### Running 50000 searches on $last objects [%]
         my $num = int rand $RAND_MAX;
         my ($obj, @others) = $storage->search({ num => $num });
-        #die 'uh oh' unless $obj && $obj->num == $num;
     }
     my $end = time;
     say {$datafile} $end-$start;
     last unless @intervals;
 }
 close $datafile;
-
-say {*STDERR} "$hit hits, $miss misses";
